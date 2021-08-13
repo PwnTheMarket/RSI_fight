3ec821ba41fc08a0807cdec67d4df8f533e0c644
David WElls <CE2wells@gmail.com>
2021-08-13 13:25:13 -0700

    initial commit

---
+++ fight_chart.py
@@ -0,0 +1,103 @@
+import matplotlib.dates as mdates
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import math
+import datetime
+
+PAN_LOOKAHEAD_X = 0.15
+PAN_LOOKAHEAD_Y = 0.05
+FRAMES = 2000
+FRAMES_SKIPPED = 0
+CHART_TEXT_COLOR = 'lawngreen'
+LINE_COLOR = 'cyan'
+CHART_BACKGROUND_COLOR = 'black'
+TRANSPARENT_COLOR = 'magenta'
+PLOT_DF = None
+
+fig, ax = plt.subplots(ncols=3, figsize=(64, 36), gridspec_kw={'width_ratios': [5, 10, 5]})
+# Init PNL Text Objects
+pnl_font = {'family': 'serif',
+            'color': 'lawngreen',
+            'weight': 'bold',
+            'size': 80,
+            }
+title_font = {'fontsize':130, 'weight':'bold', 'color':CHART_TEXT_COLOR}
+rsi_pnl_text = ax[2].text(0.01, 0.1,
+                          "RSI 1m: $0\n\nRSI 5m: $0\n\nRSI 15m: $0\n\nRSI 1h: $0\n\nRSI 1d: $0",
+                          bbox=dict(boxstyle='round', facecolor='black'),
+                          fontdict=pnl_font)
+buyhold_pnl_text = ax[0].text(0.0, 0.3, "Buy and hold: $0",
+                              bbox=dict(boxstyle='round', facecolor='black'),
+                              fontdict=pnl_font)
+line, = ax[1].plot([], [], color=LINE_COLOR, linewidth=3.0)
+
+class Chart:
+    def __init__(self, df):
+        global PLOT_DF
+        PLOT_DF = df
+        fig.tight_layout()
+        plt.subplots_adjust(top=0.9, bottom=0.05)
+        fig.patch.set_facecolor(TRANSPARENT_COLOR)
+
+        plt.rcParams.update({'font.size': 24})
+        fmt_month = mdates.MonthLocator(interval=1)
+        fmt_year = mdates.YearLocator()
+        ax[1].xaxis.set_major_locator(fmt_year)
+        ax[1].xaxis.set_minor_locator(fmt_month)
+
+        ax[1].xaxis.set_minor_formatter(mdates.DateFormatter('%m'))
+        ax[1].xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+        ax[1].tick_params(axis='both', which='minor', labelsize=30, colors=CHART_TEXT_COLOR)
+        ax[1].tick_params(axis='both', which='major', labelsize=65, colors=CHART_TEXT_COLOR)
+
+        ax[1].set_xlabel("Date", fontweight='bold', fontsize=70, color=CHART_TEXT_COLOR)
+        ax[1].set_ylabel("Price", fontweight='bold', fontsize=70, color=CHART_TEXT_COLOR)
+
+        ax[0].axis('off')
+        ax[2].axis('off')
+
+        ax[1].set_facecolor(CHART_BACKGROUND_COLOR)
+        ax[1].grid(True)
+
+
+    def draw(self):
+        global FRAMES_SKIPPED
+        total_rows = len(PLOT_DF.index)
+        FRAMES_SKIPPED = math.floor(total_rows / FRAMES)
+        anim = FuncAnimation(fig, animate, interval=50, frames=min(total_rows, FRAMES), blit=True)
+        #plt.show()
+        anim.save("{}_simpleRSI.mp4".format(PLOT_DF.iloc[0]['ticker']), writer="ffmpeg")
+
+def animate(i):
+    # Only plot line 1 every FRAMES_SKIPPED interval
+    i  = i * FRAMES_SKIPPED
+
+    # Graph PAN logic
+    total_rows = len(PLOT_DF.index)
+    look_ahead_x = i + math.floor(total_rows * PAN_LOOKAHEAD_X)
+    look_behind_x = max(0, i - math.floor(total_rows * PAN_LOOKAHEAD_X))
+    look_ahead_y = min(total_rows, i + math.floor(total_rows * PAN_LOOKAHEAD_Y))
+    look_behind_y = max(0, i - math.floor(total_rows * PAN_LOOKAHEAD_Y))
+    if look_ahead_x <= total_rows:
+        end_t = PLOT_DF.iloc[i:look_ahead_x]['t'].max()
+    else:
+        end_t = PLOT_DF.iloc[-1]['t']
+    end_c = PLOT_DF.iloc[look_behind_y:look_ahead_y]['close'].max()
+
+    start_t = PLOT_DF.iloc[look_behind_x]['t']
+    start_c = PLOT_DF['close'].min()
+
+    ax[1].set_ylim(start_c, end_c)
+    ax[1].set_xlim(start_t, end_t)
+
+    ax[1].set_title("{} - ${:.2f}".format(PLOT_DF.iloc[i]['ticker'], PLOT_DF.iloc[i]['close']), loc='center', fontdict=title_font)
+
+    rsi_pnl_text.set_text("RSI 1m: ${:.2f}\n\nRSI 5m: ${:.2f}\n\nRSI 15m: ${:.2f}\n\nRSI 1h: ${:.2f}\n\nRSI 1d: ${:.2f}"
+                          .format(PLOT_DF.iloc[i]['cash_pnl_minute_1'],PLOT_DF.iloc[i]['cash_pnl_minute_5'],
+                                  PLOT_DF.iloc[i]['cash_pnl_minute_15'], PLOT_DF.iloc[i]['cash_pnl_hour_1'],PLOT_DF.iloc[i]['cash_pnl_day_1']),
+                          )
+    buyhold_pnl_text.set_text("Buy and hold: ${}".format( round((40000 / PLOT_DF.iloc[0]['close']) * PLOT_DF.iloc[i]['close']), 2))
+    line.set_data(PLOT_DF.iloc[0:i:FRAMES_SKIPPED]['t'], PLOT_DF.iloc[0:i:FRAMES_SKIPPED]['close'])
+
+    ax[2].figure.canvas.draw()
+    return (line,rsi_pnl_text)
---
+++ rsi_research.py
@@ -0,0 +1,148 @@
+import requests
+import json
+import datetime
+import fight_chart
+import math
+import pandas as pd
+
+RSI_LENGTH = 14
+QUANDL_API_KEY = #API_KEY_HERE
+STARTING_CASH = 40000
+START_DATE = "2021-03-01"
+CHART_PERIODS = [("minute", 1), ("minute", 5), ("minute", 15), ("hour", 1), ("day", 1)]
+
+def calculate_rsi(df, rsi_type, _window):
+    """[RSI function]
+
+    Args:
+        df ([DataFrame]): [DataFrame with a column 'Close' for the close price]
+        _window ([int]): [The lookback window.](default : {14})
+        _plot ([int]): [1 if you want to see the plot](default : {0})
+        _start ([Date]):[if _plot=1, start of plot](default : {None})
+        _end ([Date]):[if _plot=1, end of plot](default : {None})
+    """
+    df.drop(df[df['close'] <= 0].index, inplace=True)
+    ##### Diff for the différences between last close and now
+    df['Diff'] = df['close'].transform(lambda x: x.diff())
+    df['Diff'] = df['Diff'].fillna(0)
+    ##### In 'Up', just keep the positive values
+    df['Up'] = df['Diff']
+    df.loc[(df['Up'] < 0), 'Up'] = 0
+    ##### Diff for the différences between last close and now
+    df['Down'] = df['Diff']
+    ##### In 'Down', just keep the negative values
+    df.loc[(df['Down'] > 0), 'Down'] = 0
+    df['Down'] = abs(df['Down'])
+
+    if rsi_type == "wilders":
+        df['avg_up' + str(_window)] = df['Up'].ewm(alpha=1.0 / _window, adjust=False).mean()
+        df['avg_down' + str(_window)] = df['Down'].ewm(alpha=1.0 / _window, adjust=False).mean()
+    else:
+        ##### Moving average on Up & Down
+        df['avg_up' + str(_window)] = df['Up'].transform(lambda x: x.rolling(window=_window).mean())
+        df['avg_down' + str(_window)] = df['Down'].transform(lambda x: x.rolling(window=_window).mean())
+
+    ##### RS is the ratio of the means of Up & Down
+    df['RS_' + str(_window)] = df['avg_up' + str(_window)] / df['avg_down' + str(_window)]
+
+    ##### RSI Calculation
+    ##### 100 - (100/(1 + RS))
+    df['rsi'] = 100 - (100 / (1 + df['RS_' + str(_window)]))
+
+    ##### Drop useless columns
+    df = df.drop(['Diff', 'Up', 'Down', 'avg_up' + str(_window), 'avg_down' + str(_window), 'RS_' + str(_window)],
+                 axis=1)
+    return df
+
+def add_rsi_pnl(ticker_df, cash_tag="cash"):
+    price_bought = None
+    shares_bought = None
+    cash_available = STARTING_CASH
+    profits_df = pd.DataFrame([])
+    # generate buy/sell signals
+    ticker_df['signal'] = ticker_df['rsi'].apply(
+        lambda rsi: 'buy' if rsi < 30 else ('sell' if rsi > 70 else 'noAction'))
+
+    # compute pnl for buy/sell signals
+    for index, row in ticker_df.iterrows():
+        if row['signal'] == 'buy' and price_bought is None:
+            price_bought = row['close']
+            remaining_cash = cash_available % price_bought
+            shares_bought = math.floor(cash_available / price_bought)
+        elif row['signal'] == 'sell' and price_bought is not None:
+            cash_available = round(remaining_cash + shares_bought*row['close'], 2)
+            price_bought = None
+        profits_df = profits_df.append({'t':row['t'], "cash_pnl_{}".format(cash_tag):cash_available}, ignore_index=True)
+    return ticker_df.set_index('t').join(profits_df.set_index('t')).reset_index()
+
+def get_ticker_data(ticker, time_series, timeseries_interval, date, rsi_type):
+    # number of days to grab for each api request
+    query_window = 100
+    # RSI (esp. wilders) needs more historical data to work with in order to
+    # generate a more precise rsi value, so move start date up.
+    start_date = datetime.datetime.strptime(date, "%Y-%m-%d") - datetime.timedelta(days=80)
+    ticker_df = pd.DataFrame([])
+    now = datetime.datetime.now()
+
+    # download stock data and build dataframe in 'query_window' amounts
+    while start_date <= now:
+        end_date = start_date + datetime.timedelta(days=query_window)
+        response = requests.get(
+            "https://api.polygon.io/v2/aggs/ticker/{}/range/{}/{}/{}/{}?unadjusted=false&sort=asc&limit=50000&apiKey={}".format(
+                ticker, timeseries_interval, time_series, start_date.strftime("%Y-%m-%d"),
+                end_date.strftime("%Y-%m-%d"), QUANDL_API_KEY))
+        json_response = json.loads(response.text)
+        stock_tmp = pd.DataFrame(json_response["results"])
+        ticker_df = ticker_df.append(stock_tmp)
+        start_date = end_date
+
+    # convert epoch to readable date and rename columns
+    ticker_df['t'] = ticker_df['t'].apply(lambda t: datetime.datetime.utcfromtimestamp(t*0.001).timestamp()).astype(int)
+    ticker_df.rename(columns={'h': 'high', 'l': 'low', 'v': 'volume', 'o': 'open', 'c': 'close'}, inplace=True)
+
+    # since we had to include previous stock dates in order to calculate RSI value for user supplied date
+    # skip these first values and only return date for user's date and on
+    # rsi_data = ticker_df[ticker_df[ticker_df['t']==date].index.values[0]-1:]
+    for index, row in ticker_df.iterrows():
+        if date == datetime.datetime.utcfromtimestamp(row['t']).strftime("%Y-%m-%d"):
+            return ticker_df[ticker_df[ticker_df['t'] == row['t']].index.values[0] - 1 :]
+
+
+def fight(ticker, date, rsi_type="simple"):
+    stock_df = None
+    try:
+        stock_df = pd.read_csv("rsi_data_{}_{}_final.pd".format(ticker, date))
+    except:
+        ticker_df = None
+        for stock_time in CHART_PERIODS:
+            try:
+                ticker_df = pd.read_csv("rsi_data_{}_{}_{}_{}.pd".format(ticker, date, stock_time[0], stock_time[1]))
+            except:
+                ticker_df = get_ticker_data(ticker, stock_time[0], stock_time[1], date, rsi_type)
+                ticker_df['ticker'] = ticker
+                # compute rsi
+                ticker_df = calculate_rsi(ticker_df, rsi_type, RSI_LENGTH)
+                ticker_df.to_csv("rsi_data_{}_{}_{}_{}.pd".format(ticker, date, stock_time[0], stock_time[1]))
+                pass
+            rsi_df = add_rsi_pnl(ticker_df, "{}_{}".format(stock_time[0], stock_time[1]))
+            # Build and join various RSI time interval pnl data to one entire dataframe
+            if stock_df is None:
+                stock_df = ticker_df
+            cols_to_use = rsi_df.columns.difference(stock_df.columns)
+
+            stock_df = pd.merge(stock_df, rsi_df[['t', cols_to_use[0]]], left_on='t', right_on='t', how="outer")
+
+        stock_df = stock_df.sort_values(by=['t'])
+        # upon merging with a superset, our subset pnl is unable to set for the first few superset ticks,
+        # this is a way to set first row to STARTING_CASH which will allow us to fillna with its value for first few rows
+        for stock_time in CHART_PERIODS:
+            stock_df.at[0, "cash_pnl_{}_{}".format(stock_time[0], stock_time[1])] = STARTING_CASH
+        stock_df = stock_df.fillna(method='ffill')
+        stock_df = stock_df[stock_df['close'].notna()]
+        stock_df.to_csv("rsi_data_{}_{}_final.pd".format(ticker, date))
+    stock_df = stock_df.drop_duplicates(subset=['t'])
+    stock_df['t'] = stock_df['t'].apply(lambda x: datetime.datetime.utcfromtimestamp(x))
+
+    fight_chart.Chart(stock_df).draw()
+
+#fight("TSLA", START_DATE, "wilders")
